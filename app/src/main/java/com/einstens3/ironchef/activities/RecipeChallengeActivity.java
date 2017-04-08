ffd092e1f722df5e06a079dc9ae7f9dcc91875f8
hideki <hideki@couchbase.com>
2017-04-08 13:31:01 -0700

    no message

---
+++ app/src/main/java/com/einstens3/ironchef/activities/RecipeChallengeActivity.java
@@ -0,0 +1,31 @@
+package com.einstens3.ironchef.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.einstens3.ironchef.R;
+
+public class RecipeChallengeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_challenge);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/einstens3/ironchef/fragments/RecipeDetailActivityFragment.java
@@ -0,0 +1,24 @@
+package com.einstens3.ironchef.fragments;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.einstens3.ironchef.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class RecipeDetailActivityFragment extends Fragment {
+
+    public RecipeDetailActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_recipe_detail, container, false);
+    }
+}
